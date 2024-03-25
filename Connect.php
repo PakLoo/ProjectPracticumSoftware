4de9938f62fde456d1c11307ed9bf365ff09d699
PakLoo <potchara.d@ku.th>
2024-03-25 23:54:00 +0700

    Update Connect.php

--- Connect.php
+++ Connect.php
@@ -1,5 +1,7 @@
 <?php
-
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 //connect database
 $servername = "151.106.124.154"; //IP เครื่อง server
 $username = "u583789277_LabdbG15"; //user account ที่กําหนดให้
