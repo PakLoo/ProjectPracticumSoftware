fe80650363b13bc113ea4d0f704d2fd15b7cf58d
dada <potchara.d@ku.th>
2024-03-25 20:39:25 +0700

    first commit

---
+++ Connect.php
@@ -0,0 +1,9 @@
+<?php
+
+//connect database
+$servername = "151.106.124.154"; //IP เครื่อง server
+$username = "u583789277_LabdbG15"; //user account ที่กําหนดให้
+$password = "Chairman2567"; //รหัสผ่านที่กําหนดให้
+$dbname = "u583789277_LabdbG15"; //ชื่อฐานข้อมูลที่กําหนดให้
+
+?>
