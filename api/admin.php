17d77e0c672ac433238930d7b13cc721e2ca97c5
gugolfpabin <161675245+gugolfpabin@users.noreply.github.com>
2024-03-27 02:45:36 +0700

    aa

--- api/admin.php
+++ api/admin.php
@@ -22,3 +22,143 @@ $app->get('/members', function (Request $request, Response $response, array $arg
 
 
 });
+
+
+<<<<<<< HEAD
+//14
+$app->post('/EventInsert', 
+        function (Request $request, Response $response, array $args){
+$conn = $GLOBALS['conn'];
+$body = $request->getParsedBody();
+$stmt = $conn->prepare("insert into event " . "(EventID,EventName, EventDate, EventDateEnd)". 
+                        " values (?,?,?,?)");
+$stmt->bind_param("isss",
+                    $body['EventID'],$body['EventName'], $body['EventDate'], $body['EventDateEnd']);
+=======
+//ลบข้อมูลโซน เฉพาะรายการของชื่อโซน
+$app->post("/DeleteZone",function (Request $request,   Response $response,array $args) {
+    $body= $request->getParsedBody();
+    $conn = $GLOBALS["conn"];
+    $stmt = $conn->prepare("DELETE FROM Zone WHERE ZoneName=?");
+    $stmt->bind_param("s",$body['ZoneName']);
+    $stmt->execute();
+    $result = $stmt->affected_rows;
+    $response->getBody()->write($result."");
+    return $response->withHeader("Content - Type","application/json");
+});
+
+
+//ลบข้อมูลบูธ เฉพาะรายการของชื่อบูธ
+$app->post("/DeleteBooth",function (Request $request,   Response $response,array $args) {
+    $body= $request->getParsedBody();
+    $conn = $GLOBALS["conn"];
+    $stmt = $conn->prepare("DELETE FROM Zone WHERE BoothName=?");
+    $stmt->bind_param("s",$body['BoothName']);
+    $stmt->execute();
+    $result = $stmt->affected_rows;
+    $response->getBody()->write($result."");
+    return $response->withHeader("Content - Type","application/json");
+});
+
+
+//เพิ่มข้อมูลบูธ
+$app->post('/InsertDetailBooth', 
+        function (Request $request, Response $response, array $args){
+$conn = $GLOBALS['conn'];
+$body = $request->getParsedBody();
+$stmt = $conn->prepare("INSERT INTO members " . "(BoothID , BoothName, BoothSize)".
+                        " values (?,?,?)".
+                        "INSERT INTO Booking " . "(BoothSelling)".
+                        " values (?)".
+                        "INSERT INTO Zone " . "(ZoneID)".
+                        " values (?);");
+$stmt->bind_param("isssi",
+                    $body['BoothID'], $body['BoothName'], $body['BoothSize'],
+                    $body['BoothSelling'], $body['ZoneID']);
+>>>>>>> 71e23a0691502547bfbb9e4c70f8e1252e4429e5
+$stmt->execute();
+$result = $stmt->affected_rows;
+$response->getBody()->write($result."");
+return $response->withHeader('Content-Type', 'application/json');
+<<<<<<< HEAD
+echo "Insert Success";
+
+});
+//แสดงข้อมูล * ใน Event
+$app->get('/EventSelect', function (Request $request, Response $response, array $args){
+    $conn = $GLOBALS['conn'];
+    $sql = "select * from event";
+    $result = $conn->query($sql);
+    $data = array();
+    while($row = $result->fetch_assoc()){
+        array_push($data, $row);
+    }
+    $json = json_encode($data);
+    $response->getBody()->write($json);
+    return $response->withHeader('Content-Type', 'application/json');
+});
+//15
+$app->post('/EventUpdate', 
+        function (Request $request, Response $response, array $args){
+$conn = $GLOBALS['conn'];
+$body = $request->getParsedBody();
+$stmt = $conn->prepare("UPDATE event SET EventName=?,EventDate=?,EventDateEnd=? where EventID=?" );
+$stmt->bind_param("ssss",
+                    $body['EventName'], $body['EventDate'], $body['EventDateEnd'], $body['EventID']);
+$stmt->execute();
+$result = $stmt->affected_rows;
+$response->getBody()->write($result."");
+return $response->withHeader('Content-Type', 'application/json');
+echo "Update Success";
+
+});
+//16
+$app->post('/ZoneInsert', 
+        function (Request $request, Response $response, array $args){
+$conn = $GLOBALS['conn'];
+$body = $request->getParsedBody();
+$stmt = $conn->prepare("insert into Zone " . "(ZoneQuantity,ZoneName, ZoneDetail)". 
+                        " values (?,?,?)");
+$stmt->bind_param("sss",
+                     $body['ZoneQuantity'],$body['ZoneName'], $body['ZoneDetail']);
+$stmt->execute();
+$result = $stmt->affected_rows;
+$response->getBody()->write($result."");
+return $response->withHeader('Content-Type', 'application/json');
+echo "Insert Success";
+
+});
+//selectZone
+$app->get('/ZoneSelect', function (Request $request, Response $response, array $args){
+    $conn = $GLOBALS['conn'];
+    $sql = "select * from Zone";
+    $result = $conn->query($sql);
+    $data = array();
+    while($row = $result->fetch_assoc()){
+        array_push($data, $row);
+    }
+    $json = json_encode($data);
+    $response->getBody()->write($json);
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
+=======
+});
+
+//แก้ไขข้อมูลบูธ
+$app->post("/EditBooth",function (Request $request,   Response $response,array $args) {
+    $body= $request->getParsedBody();
+    $conn = $GLOBALS["conn"];
+    $stmt = $conn->prepare("UPDATE Booth 
+                            INNER JOIN Booking on Booking.BoothID = Booth.BoothID 
+                            INNER JOIN Zone on Zone.BoothID = Booth.BoothID 
+                            set BoothName = ? ,BoothSize = ? ,BoothSelling = ? ,ZoneID  = ? where BoothName = ?");
+    $stmt->bind_param("sssss",$body['BoothName'],$body['BoothSize'],$body['BoothSelling'],$body['ZoneID']);
+    $stmt->execute();
+    $result = $stmt->affected_rows;
+    $response->getBody()->write($result."");
+    return $response->withHeader("Content - Type","application/json");
+});
+
+?>
+>>>>>>> 71e23a0691502547bfbb9e4c70f8e1252e4429e5
