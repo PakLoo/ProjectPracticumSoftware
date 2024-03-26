bf4693de84ee9e6bf76ca204a53ede38cd68cc33
PakLoo <potchara.d@ku.th>
2024-03-26 18:32:22 +0700

    g

--- api/member.php
+++ api/member.php
@@ -9,20 +9,20 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 
 
 //select all members
-// $app->get('/members', function (Request $request, Response $response, array $args){
-//     $conn = $GLOBALS['conn'];
-//     $sql = "select * from members";
-//     $result = $conn->query($sql);
-//     $data = array();
-//     while($row = $result->fetch_assoc()){
-//         array_push($data, $row);
-//     }
-//     $json = json_encode($data);
-//     $response->getBody()->write($json);
-//     return $response->withHeader('Content-Type', 'application/json');
+$app->get('/members', function (Request $request, Response $response, array $args){
+    $conn = $GLOBALS['conn'];
+    $sql = "select * from members";
+    $result = $conn->query($sql);
+    $data = array();
+    while($row = $result->fetch_assoc()){
+        array_push($data, $row);
+    }
+    $json = json_encode($data);
+    $response->getBody()->write($json);
+    return $response->withHeader('Content-Type', 'application/json');
 
 
-// });
+});
 
 $app->post("/userLogin",function (Request $request,Response $response,array $args) { 
     function getPasswordFromDB($conn,$email){
