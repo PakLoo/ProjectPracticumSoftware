116ecd55003bc44f67e3dfe55b24674245640f95
gugolfpabin <161675245+gugolfpabin@users.noreply.github.com>
2024-03-27 02:39:19 +0700

    Update admin.php

--- api/admin.php
+++ api/admin.php
@@ -22,3 +22,79 @@ $app->get('/members', function (Request $request, Response $response, array $arg
 
 
 });
+
+
+//14
+$app->post('/EventInsert', 
+        function (Request $request, Response $response, array $args){
+$conn = $GLOBALS['conn'];
+$body = $request->getParsedBody();
+$stmt = $conn->prepare("insert into event " . "(EventID,EventName, EventDate, EventDateEnd)". 
+                        " values (?,?,?,?)");
+$stmt->bind_param("isss",
+                    $body['EventID'],$body['EventName'], $body['EventDate'], $body['EventDateEnd']);
+$stmt->execute();
+$result = $stmt->affected_rows;
+$response->getBody()->write($result."");
+return $response->withHeader('Content-Type', 'application/json');
+echo "Insert Success";
+
+});
+//แสดงข้อมูล * ใน Event
+$app->get('/EventSelect', function (Request $request, Response $response, array $args){
+    $conn = $GLOBALS['conn'];
+    $sql = "select * from event";
+    $result = $conn->query($sql);
+    $data = array();
+    while($row = $result->fetch_assoc()){
+        array_push($data, $row);
+    }
+    $json = json_encode($data);
+    $response->getBody()->write($json);
+    return $response->withHeader('Content-Type', 'application/json');
+});
+//15
+$app->post('/EventUpdate', 
+        function (Request $request, Response $response, array $args){
+$conn = $GLOBALS['conn'];
+$body = $request->getParsedBody();
+$stmt = $conn->prepare("UPDATE event SET EventName=?,EventDate=?,EventDateEnd=? where EventID=?" );
+$stmt->bind_param("ssss",
+                    $body['EventName'], $body['EventDate'], $body['EventDateEnd'], $body['EventID']);
+$stmt->execute();
+$result = $stmt->affected_rows;
+$response->getBody()->write($result."");
+return $response->withHeader('Content-Type', 'application/json');
+echo "Update Success";
+
+});
+//16
+$app->post('/ZoneInsert', 
+        function (Request $request, Response $response, array $args){
+$conn = $GLOBALS['conn'];
+$body = $request->getParsedBody();
+$stmt = $conn->prepare("insert into Zone " . "(ZoneQuantity,ZoneName, ZoneDetail)". 
+                        " values (?,?,?)");
+$stmt->bind_param("sss",
+                     $body['ZoneQuantity'],$body['ZoneName'], $body['ZoneDetail']);
+$stmt->execute();
+$result = $stmt->affected_rows;
+$response->getBody()->write($result."");
+return $response->withHeader('Content-Type', 'application/json');
+echo "Insert Success";
+
+});
+//selectZone
+$app->get('/ZoneSelect', function (Request $request, Response $response, array $args){
+    $conn = $GLOBALS['conn'];
+    $sql = "select * from Zone";
+    $result = $conn->query($sql);
+    $data = array();
+    while($row = $result->fetch_assoc()){
+        array_push($data, $row);
+    }
+    $json = json_encode($data);
+    $response->getBody()->write($json);
+    return $response->withHeader('Content-Type', 'application/json');
+});
+
