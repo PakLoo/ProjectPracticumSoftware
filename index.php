624d79fa9549ba1ab3947cd0887409205268838a
PakLoo <potchara.d@ku.th>
2024-03-26 18:12:40 +0700

    g

--- index.php
+++ index.php
@@ -10,13 +10,26 @@ use Slim\Factory\AppFactory;
 require __DIR__ . '/vendor/autoload.php';
 
 $app = AppFactory::create();
-$app->setBasePath('/ProjectPracticumSoftware/api');
+$app->setBasePath('/ProjectPracticumSoftware');
 
 require __DIR__ . '/Connect.php';
 require __DIR__ . '/api/General.php';
 require __DIR__ . '/api/member.php';
 require __DIR__ . '/api/admin.php';
 
+$app->get('/members', function (Request $request, Response $response, array $args){
+    $conn = $GLOBALS['conn'];
+    $sql = "select * from members";
+    $result = $conn->query($sql);
+    $data = array();
+    while($row = $result->fetch_assoc()){
+        array_push($data, $row);
+    }
+    $json = json_encode($data);
+    $response->getBody()->write($json);
+    return $response->withHeader('Content-Type', 'application/json');
 
+
+});
 
 $app->run();
